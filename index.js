7956d21928cdf65605943817a79fb238a19f2253
FrancoItr <1francoiturri@gmail.com>
2023-05-22 03:54:35 -0300

    Mi segundo ejercicio de Open Bootcamp

---
+++ index.js
@@ -0,0 +1,22 @@
+const estudiante = [
+    Nombre = "Franco",
+    Edad = 30,
+    Desarrollador = true,
+    Nacimiento = new Date(1992, 9, 12),
+    Libro = {
+        titulo : "Las enseñanzas de Don Juan",
+        autor : "Carlos Castañeda",
+        fecha : 19685,
+        URL_ : "https.es.wikipedia.org/wiki/Las_ense%C3%B1anzas_de_Don_Juan"
+    }
+];
+
+// const estudiante = [
+//     Franco.Nombre,
+//     Franco.Edad,
+//     "Desarrollador: " + Franco.desarrollador,
+//     "Nacimiento: " + Franco.nacimiento,
+//     "Libro Favorito :" + Franco.libro(titulo, autor, fecha, URL_)
+// ];
+
+console.log(estudiante);
